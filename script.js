067833e19e42a40dd3623863b22eae5bc3294e6c
Kate Travers <ktravers@github.com>
2021-02-13 16:09:16 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,34 @@
+// Button click event handlers
+$(".bedtime").click(function() {
+    $("img").hide();
+    $(".sleep").show();
+    $("body").css("background-color", "darkblue");
+});
+
+$(".pet-the-cat").click(function() {
+    $("img").hide();
+    $(".pets").show();
+});
+
+$(".feed-the-cat").click(function() {
+    $("img").hide();
+    $(".pizza").show();
+});
+
+$(".play-music").click(function() {
+    $("img").hide();
+    $(".party").show();
+});
+
+// Easter egg! Double click on Pusheen
+$("img").dblclick(function () {
+    $("img").hide();
+    $(".love").show();    
+});
+
+// Reset to default 
+$(".actions").mouseenter(function() {
+    $("img").hide();
+    $(".default").show();
+    $("body").css("background-color", "#FBF0E7");
+});
